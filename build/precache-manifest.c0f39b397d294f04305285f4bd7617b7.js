f6228486be576c414fa1ea3993959fcedd5ef8bf
Joseph Lee <jin3lee@gmail.com>
2019-10-03 22:35:39 -0700

    change favicon.ico

---
+++ build/precache-manifest.c0f39b397d294f04305285f4bd7617b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62216b8bce2439910aba58a688938bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e5ca8658c4eb3c893e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "090046e86df800e89e6f",
+    "url": "/static/js/2.3cfe90df.chunk.js"
+  },
+  {
+    "revision": "72e5ca8658c4eb3c893e",
+    "url": "/static/js/main.c29dc6cf.chunk.js"
+  },
+  {
+    "revision": "bc04c75a9663823cb54d",
+    "url": "/static/js/runtime-main.a9e2c26e.js"
+  }
+]);
