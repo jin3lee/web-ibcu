bfcbfe198864999fc187ef6ca3b7e2cbb5259735
Joseph Lee <jin3lee@gmail.com>
2019-10-05 19:20:35 -0700

    add image

--- src/Pages/HomePage/components/ExperienceGallery.js
+++ src/Pages/HomePage/components/ExperienceGallery.js
@@ -7,6 +7,17 @@ class ExperienceGallery extends React.Component {
   render() {
     return(
       <div style={ style.container }>
+        <div style={{ marginLeft: '6%' }}>
+          <div style={{ display: "flex" }}>
+            <div style={{ flex: 1, fontSize: '1.5em', color: '#0C54A3', fontWeight: 'bold', backgroundColor:"#E4EBF3", paddingLeft: 15, paddingRight: 15, paddingTop:8, paddingBottom:8 }}>
+              #IbcuExperience
+            </div>
+            <div style={{ flex: 1.5 }}/>
+          </div>
+          <div style={{ fontSize: '4em', fontWeight: 'bold', color: 'white' }}>
+            Hiking Mountains
+          </div>
+        </div>
       </div>
     );
   }
@@ -14,14 +25,13 @@ class ExperienceGallery extends React.Component {
 
 const style = {
   container: {
-    overflow: "hidden",
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "black",
     height: 450,
-    backgroundImage: 'url(' + imageTree + ')',
+    display: 'flex',
+    overflow: "hidden",
+    alignItems: 'top',
+    justifyContent: 'left',
     backgroundPosition: 'center',
+    backgroundImage: 'url(' + imageTree + ')',
   }
 }
 
