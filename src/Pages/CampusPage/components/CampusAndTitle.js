8bffbfe2a2fd582f8a73c299d60cd8572889f4da
Joseph Lee <jin3lee@gmail.com>
2019-10-06 10:09:13 -0700

    change campusImage in campusPage to be cover for backgroundSize

--- src/Pages/CampusPage/components/CampusAndTitle.js
+++ src/Pages/CampusPage/components/CampusAndTitle.js
@@ -7,7 +7,7 @@ class CampusAndTitle extends React.Component {
   render() {
     return(
       <div style={ style.container }>
-        <div style={{ marginLeft: '6%', alignItems:'bottom', justifyContent:'bottom', marginTop: 70 }}>
+        <div style={{ marginLeft: '6%', alignItems:'bottom', justifyContent:'bottom', marginTop: "10%" }}>
           <div style={{ fontSize: '3em', fontWeight: 'bold', color: 'white', textShadow: '-1px 1px 10px rgba(0, 0, 0, 0.75)' }}>
             Discover International Bible College & University
           </div>
@@ -22,8 +22,9 @@ const style = {
   container: {
     height: 250,
     display: 'flex',
-    overflow: "hidden",
     alignItems: 'top',
+    overflow: "hidden",
+    backgroundSize: 'cover',
     justifyContent: 'center',
     backgroundPosition: 'center',
     backgroundImage: 'url(' + campusImage + ')',
