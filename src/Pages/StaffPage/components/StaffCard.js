c2b4a326f1538e4e7de678d03c8a3f37e1404590
Joseph Lee <jin3lee@gmail.com>
2019-10-12 21:43:52 -0700

    add profile photos of faculty&Staff

---
+++ src/Pages/StaffPage/components/StaffCard.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { connect } from 'react-redux';
+
+class StaffCard extends React.Component {
+
+  constructor( props ) {
+    super( props );
+  }
+
+  render() {
+    return(
+      <div style={ style.container }>
+        <div style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+          <img src={ this.props.imgSrc } style={ style.logoStyle } alt="staffPhoto"/>
+        </div>
+        <div style={{
+                      flex: 2,
+                      display: 'flex',
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      marginLeft: 20
+                    }}>
+          <div style={{ marginBottom: 2, fontSize: 20, fontFamily: 'Avenir', fontWeight: 'bold'}}>
+            { this.props.firstName } { this.props.lastName }
+          </div>
+          <div style={{ color: '#0C54A3', fontSize: 14}}>
+            { this.props.title }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const style = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'left',
+    width: 300,
+    height: 100,
+  },
+  logoStyle:{
+    width: 80,
+    height: 80,
+  },
+}
+
+export default ( StaffCard );
--- src/Pages/StaffPage/components/StaffContent.js
+++ src/Pages/StaffPage/components/StaffContent.js
@@ -4,6 +4,21 @@ import { connect } from 'react-redux';
 import { updatePage } from '../../../Container/actions.js';
 import { PAGE_ID_HOME, PAGE_ID_STAFF } from '../../../Container/actionTypes.js'
 
+import bankMike from './../../../assets/staff/staff_icon/Banks_Mike.png';
+import changChoonWon from './../../../assets/staff/staff_icon/Chang_Choon_Won.png';
+import debayJack from './../../../assets/staff/staff_icon/DeBay_Jack.png';
+import debayShirl from './../../../assets/staff/staff_icon/DeBay_Shirl.png';
+import hanYongSeok from './../../../assets/staff/staff_icon/Han_Yong_Seok.png';
+import kimPaul from './../../../assets/staff/staff_icon/Kim_Paul.png';
+import koChongCheh from './../../../assets/staff/staff_icon/Ko_Chong_Cheh.png';
+import kongRichard from './../../../assets/staff/staff_icon/Kong_Richard.png';
+import kyungEzra from './../../../assets/staff/staff_icon/Kyung_Ezra.png';
+import southcottKen from './../../../assets/staff/staff_icon/Southcott_Ken.png';
+import tarsiukTerry from './../../../assets/staff/staff_icon/Tarsiuk_Terry.png';
+import defaultIcon from './../../../assets/staff/staff_icon/default.png';
+
+import StaffCard from './StaffCard.js';
+
 class StaffContent extends React.Component {
 
   constructor( props ) {
@@ -19,7 +34,7 @@ class StaffContent extends React.Component {
     return(
       <div style={ style.container }>
 
-        <div style={{ width: 400,marginLeft: '10%', marginTop: 30 }}>
+        <div style={{ width: 400, marginLeft: '10%', marginTop: 30 }}>
           <div style={{ display: 'flex', flexDirection: 'row', color: 'rgb(90, 90, 90)' }}>
             <button onClick={ () => { this._updatePageTo( PAGE_ID_HOME ) } } style={ style.backFlowButtonStyle }>
               Home
@@ -31,10 +46,21 @@ class StaffContent extends React.Component {
               Faculty & Staff
             </button>
           </div>
-
-
         </div>
 
+        <div style={{ display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'center'}}>
+          <StaffCard firstName="Richard" lastName="Kong" title="Board of Director & President & Secretary" imgSrc={ kongRichard }/>
+          <StaffCard firstName="Yong Seok" lastName="Han" title="Board of Director & Honorary Chairman & Instructor" imgSrc={ hanYongSeok }/>
+          <StaffCard firstName="Chong Cheh" lastName="Ko" title="Board of Director & Treasurer" imgSrc={ koChongCheh }/>
+          <StaffCard firstName="Mike" lastName="Banks" title="Instructor" imgSrc={ bankMike }/>
+          <StaffCard firstName="Choon Won" lastName="Chang" title="Instructor" imgSrc={ changChoonWon }/>
+          <StaffCard firstName="Jack" lastName="DeBay" title="Instructor" imgSrc={ debayJack }/>
+          <StaffCard firstName="Shirl" lastName="DeBay" title="Instructor" imgSrc={ debayShirl }/>
+          <StaffCard firstName="Paul" lastName="Kim" title="Instructor" imgSrc={ kimPaul }/>
+          <StaffCard firstName="Ezra" lastName="Kyung" title="Instructor" imgSrc={ kyungEzra }/>
+          <StaffCard firstName="Ken" lastName="Southcott" title="Instructor" imgSrc={ southcottKen }/>
+          <StaffCard firstName="Esther" lastName="Jang" title="Director of Admissions and Records" imgSrc={ defaultIcon }/>
+        </div>
       </div>
     );
   }
@@ -46,7 +72,7 @@ const style = {
     flexDirection: 'row',
     backgroundColor: 'white',
     flexWrap: 'wrap',
-    justifyContent: 'center'
+    justifyContent: 'left'
   },
   backFlowButtonStyle: {
     marginRight: 10,
