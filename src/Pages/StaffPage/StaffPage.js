4548635478f3b831a7524b7d6be2dbe610a3b494
Joseph Lee <jin3lee@gmail.com>
2020-12-09 02:50:56 -0600

    fix routing on faculty and staff

--- src/Pages/StaffPage/StaffPage.js
+++ src/Pages/StaffPage/StaffPage.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import StaffAndTitle from './components/StaffAndTitle.js';
 import StaffContent from './components/StaffContent.js';
-import StaffDetail from './components/StaffDetail.js';
 
 class StaffPage extends React.Component {
   render() {
@@ -11,12 +10,7 @@ class StaffPage extends React.Component {
       <div className="root-container-background" style={ style.homePageStyle }>
           <div style={{ background: 'linear-gradient(to bottom, rgb(0, 0, 60), rgb(0, 0, 0))' }}>
             <StaffAndTitle />
-            <Router>
-              <Switch>
-                <Route path="/staff" exact component={ StaffContent } />
-                <Route path="/staff/:id" exact component={ StaffDetail } />
-              </Switch>
-            </Router>
+            <StaffContent />
           </div>
       </div>
     );
