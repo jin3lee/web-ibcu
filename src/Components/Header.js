398317fa387d989203db477a615ad4462cab2f5f
Joseph Lee <jin3lee@gmail.com>
2021-01-05 13:42:02 -0600

    hide language when screen is small

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,45 +9,110 @@ import { Link } from "react-router-dom";
 
 class Header extends React.Component {
 
+  constructor(props){
+    super(props);
+    this.state = {
+      screenWidth: null
+    };
+  }
+
   render() {
-    return(
-      <header className="header-background">
-        
-        <div style={{ 
-          display: 'flex', 
-          flexDirection: 'row', 
-          justifyContent: 'space-between',
-        }}>
+
+    let screenWidth = this.state.screenWidth;
+
+    if( this.props.pageDimensions && screenWidth !== this.props.pageDimensions.width ) 
+    {
+      screenWidth = this.props.pageDimensions.width;
+    }
+
+    if( screenWidth < 515 ) {
+      console.log('screenWidth', screenWidth, this.state.screenWidth < 515 );
+      return(
+        <header className="header-background">
           
-          <div style={ style.header_style }>
+          <div style={{ 
+            display: 'flex', 
+            flexDirection: 'column', 
+          }}>
             
             <div style={{ 
-              display: 'flex',  
-              justifyContent: 'center',
-              flexDirection: 'column',
-              margin: 5,
+              display: 'flex',
+              width: '70%', 
               marginLeft: '5%',
+              marginRight: '5%',
+              paddingTop: '3%',
+              paddingBottom: '1%',
             }}>
-              <div>
-                <Link to="/">
-                  <WimuLogo style={ style.logoStyle } />
-                </Link>
+              
+              <div style={{ 
+                display: 'flex',  
+                justifyContent: 'center',
+                flexDirection: 'column',
+              }}>
+                <div>
+                  <Link to="/">
+                    <WimuLogo style={ style.logoStyle } />
+                  </Link>
+                </div>
               </div>
-            </div>
 
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+              <div style={{ width: '5%' }}/>
+
               <Link to="/" style={ style.collegeNameStyle }>
                 { this.props.translation.Header.schoolName }
               </Link>
             </div>
           </div>
 
-          
+              {/* TODO
           <TranslationToggle />
-        </div>
+            <div style={{ paddingBottom: 10 }} />
+              */}
+        </header>
+      );
 
-      </header>
-    );
+
+    /* CASE: Screen is greater than 515 */
+    } else {
+      return(
+        <header className="header-background">
+          
+          <div style={{ 
+            display: 'flex', 
+            flexDirection: 'row', 
+            justifyContent: 'space-between',
+          }}>
+            
+            <div style={ style.header_style }>
+              
+              <div style={{ 
+                display: 'flex',  
+                justifyContent: 'center',
+                flexDirection: 'column',
+                margin: 5,
+                marginLeft: '5%',
+              }}>
+                <div>
+                  <Link to="/">
+                    <WimuLogo style={ style.logoStyle } />
+                  </Link>
+                </div>
+              </div>
+
+              <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                <Link to="/" style={ style.collegeNameStyle }>
+                  { this.props.translation.Header.schoolName }
+                </Link>
+              </div>
+            </div>
+
+            
+            <TranslationToggle />
+          </div>
+
+        </header>
+      );
+    }
   }
 }
 
@@ -76,9 +141,10 @@ const style = {
   },
 }
 
-const mapStateToProps = (state, ownProps) => {
+const mapStateToProps = (state, ownProps) => {  
   return {
-    translation: state.translationToggle.translation
+    translation: state.translationToggle.translation,
+    pageDimensions: state.pageDimensions
   };
 };
 
