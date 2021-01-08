641948b9a5f5a271aa37ca68fbb89a826e99e176
Joseph Lee <jin3lee@gmail.com>
2021-01-08 01:57:19 -0600

    add side bar

--- src/Container/RootContainer.js
+++ src/Container/RootContainer.js
@@ -21,6 +21,10 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // listeners
 import PageDimensions from './../Components/PageDimensions/PageDimensions.js';
 
+
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+
 class RootContainer extends React.Component {
 
   render() {
@@ -30,19 +34,45 @@ class RootContainer extends React.Component {
 
           <Router>
             <PageDimensions />
-            <Header />
-            <Navigator />
-              <Switch>
-                <Route path="/" exact component={ HomePage } />
-                <Route path="/home" exact component={ HomePage } />
-                <Route path="/about" exact component={ AboutPage } />
-                <Route path="/academics" exact component={ AcademicsPage } />
-                <Route path="/apply" exact component={ ApplyPage } />
-                <Route path="/campus" exact component={ CampusPage } />
-                <Route path="/staff" exact component={ StaffPage } />
-                <Route path="/staff/:id" exact component={ StaffDetail } />
-              </Switch>
-            <Footer disableWhiteBuffer={ true } />
+            
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+
+              <div>
+                <ProSidebar>
+                  <Menu iconShape="square">
+                      <MenuItem>HOME</MenuItem>
+                      <MenuItem>ABOUT</MenuItem>
+                      <MenuItem>ACADEMICS</MenuItem>
+                      <MenuItem>APPLY</MenuItem>
+                      <MenuItem>CAMPUS</MenuItem>
+                      <MenuItem>FACULTY & STAFF</MenuItem>
+                    
+                      <SubMenu title="SETTINGS">
+                        <SubMenu title="Language">
+                          <MenuItem>English</MenuItem>
+                          <MenuItem>Korean</MenuItem>
+                        </SubMenu>
+                      </SubMenu>
+                  </Menu>
+                </ProSidebar>
+              </div>
+
+              <div>
+                <Header />
+                <Navigator />
+                  <Switch>
+                    <Route path="/" exact component={ HomePage } />
+                    <Route path="/home" exact component={ HomePage } />
+                    <Route path="/about" exact component={ AboutPage } />
+                    <Route path="/academics" exact component={ AcademicsPage } />
+                    <Route path="/apply" exact component={ ApplyPage } />
+                    <Route path="/campus" exact component={ CampusPage } />
+                    <Route path="/staff" exact component={ StaffPage } />
+                    <Route path="/staff/:id" exact component={ StaffDetail } />
+                  </Switch>
+                <Footer disableWhiteBuffer={ true } />
+              </div>
+            </div>
           </Router>
       </div>
     );
